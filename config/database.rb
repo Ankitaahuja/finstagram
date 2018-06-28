ae22d81d732d4bf2bdcb96354fc0b55f7305a491
Ankita Ahuja <ankitaahuja111@gmail.com>
2018-06-28 23:36:26 +0000

    Readies app for deployment

--- config/database.rb
+++ config/database.rb
@@ -1,4 +1,6 @@
+configure do
 if Sinatra::Application.development?
+     ActiveRecord::Base.logger = Logger.new(STDOUT)
   set :database, {
   adapter: "sqlite3",
   database: "db/db.sqlite3"
@@ -14,18 +16,6 @@ else
   encoding: "utf8"
   }
 end
-
-
-configure do
-  # Log queries to STDOUT in development
-  if Sinatra::Application.development?
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-  end
-
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
