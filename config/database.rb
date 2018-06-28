1bdbed4203f1f1a5a543af0af5293ccacbb6661b
Ankita Ahuja <ankitaahuja111@gmail.com>
2018-06-28 23:25:52 +0000

    Readies app for deployment

--- config/database.rb
+++ config/database.rb
@@ -1,3 +1,21 @@
+if Sinatra::Application.development?
+  set :database, {
+  adapter: "sqlite3",
+  database: "db/db.sqlite3"
+  }
+else
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://xawzlemusybbgj:3492138090d2ef8bef0a35890acd1ffea30ba60b03aba2b6fbe586ac37374170@ec2-54-235-196-250.compute-1.amazonaws.com:5432/d6dns20jeipmko')
+  set :database, {
+  adapter: "postgresql",
+  host: db.host,
+  username: db.user,
+  password: db.password,
+  database: db.path[1..-1],
+  encoding: "utf8"
+  }
+end
+
+
 configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
